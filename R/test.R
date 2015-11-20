984cb73e60d228287e29917f430b845ce469dccd
Cheng <jucheng1992@gmail.com>
2015-11-20 13:31:37 -0800

    clear some temp file

--- R/test.R
+++ R/test.R
@@ -12,8 +12,10 @@ w <- matrix(rnorm(n * p), n, p)
 a <- sample(c(0,1), n, replace = TRUE)
 y <- sample(c(0,1), n, replace = TRUE)
 gn1 <- runif(n)
+
 tmle(logit(QnA1), logit(QnA0), w, a, y, gn1,
       param = "ATE")
+
 ctmle(logit(QnA1), logit(QnA0), w, a, y, v=5, gbounds=c(0.025,0.095), patience =p, param = "Mean0")
 ctmle(logit(QnA1), logit(QnA0), w, a, y, v=5, gbounds=c(0.025,0.095), patience =p, param = "Mean1")
 ctmle(logit(QnA1), logit(QnA0), w, a, y, v=5, gbounds=c(0.025,0.095), patience =p, param = "ATE")
