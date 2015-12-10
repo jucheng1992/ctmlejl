059e5aee5e06dad456cd4a2a3ec41388196c1ba1
Cheng <jucheng1992@gmail.com>
2015-12-09 20:53:05 -0800

    change back

--- R/load-jl-package.R
+++ R/load-jl-package.R
@@ -1,12 +1,10 @@
-#' @import rjulia
-
-".onLoad" <- function(lib, pkg){
-      julia_init() 
-      julia_void_eval("using DataArrays")
-      julia_void_eval("using NumericExtensions")
-      julia_void_eval("using Distributions")
-      julia_void_eval("using TargetedLearning")
-      julia_void_eval("using DataFrames")
-      julia_void_eval("using GLM")
-      julia_void_eval("import StatsBase.predict")
-}
+# load rjulia package, initialize julia, and install related packages.
+library(rjulia)
+julia_init() 
+julia_void_eval("using DataArrays")
+julia_void_eval("using NumericExtensions")
+julia_void_eval("using Distributions")
+julia_void_eval("using TargetedLearning")
+julia_void_eval("using DataFrames")
+julia_void_eval("using GLM")
+julia_void_eval("import StatsBase.predict")
